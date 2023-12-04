bbd9e3eca5cd8ede4ef864f4fadafe0263665f05
Alexandre Osovski <74330383+AlexandreOsovski@users.noreply.github.com>
2023-12-04 15:39:13 -0300

    teste scripts

--- backoffice/src/public/includes/head.php
+++ backoffice/src/public/includes/head.php
@@ -16,13 +16,13 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
 
   <!-- Nucleo Icons -->
-  <link href="backoffice\src\public\assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="backoffice\src\public\assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="/assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <link href="backoffice\src\public\assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="/assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- CSS Files -->
-  <link id="pagestyle" href="backoffice\src\public\assets/css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
+  <link id="pagestyle" href="/assets/css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
 
   <!-- Bootstrap core JavaScript -->
   <script src="../vendor/jquery/jquery.min.js"></script>
