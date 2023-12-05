248bb24f55cf2f92a5e69f573513f870d12d5ea7
AlexandreOsovski <alexandreosovski1@gmail.com>
2023-12-05 15:52:15 +0000

    retirando session

--- backoffice/src/controller/Dashboard.php
+++ backoffice/src/controller/Dashboard.php
@@ -12,11 +12,11 @@ class Dashboard
     {
         $this->core = new core;
 
-        session_start();
-        if (empty($_SESSION)) {
-            session_destroy();
-            $this->core->redirect('/');
-        }
+       // session_start();
+       // if (empty($_SESSION)) {
+           // session_destroy();
+           // $this->core->redirect('/');
+        //}
     }
     public function index()
     {
