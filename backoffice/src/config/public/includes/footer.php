be519e41cfcaefc7e47076a264eb7b8312c6e189
Alexandre Osovski <74330383+AlexandreOsovski@users.noreply.github.com>
2023-12-06 11:37:26 -0300

    style: ajustes na dasboard

--- backoffice/src/config/public/includes/footer.php
+++ backoffice/src/config/public/includes/footer.php
@@ -1,4 +1,4 @@
-<?php include(__DIR__ . 'backoffice\src\config\public\includes\configuracaovisual.php'); ?>
+<?php //include(__DIR__ . 'backoffice\src\config\public\includes\configuracaovisual.php'); ?>
 <script src="backoffice\src\config/public/assets/js/argon-dashboard.min.js?v=2.0.4"></script>
 <script src="<?php include('backoffice/src/config/public/includes/scripts.js') ?>"></script>
 <footer class="sticky-footer bg-gray-100 mb-2 mt-2">
